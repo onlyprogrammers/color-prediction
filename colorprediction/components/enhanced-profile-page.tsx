51b90866096b4fd651cd9c1dfa0a4b55d3b69628
onlyprogrammers <toajeetrathore@gmail.com>
2024-11-07 20:16:42 +0530

    update profile page

--- colorprediction/components/enhanced-profile-page.tsx
+++ colorprediction/components/enhanced-profile-page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from "react"
 import Image from "next/image"
 import { Bell, ChevronRight, Gift, History, LayoutGrid, LogOut, Settings, User, Wallet } from "lucide-react"
 import { Button } from "@/components/ui/button"
